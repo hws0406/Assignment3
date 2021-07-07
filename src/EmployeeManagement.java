62424f5bbf643f2b3bc85ab4ad7401c6500a1cda
Ho Wee Sern <72723621+hws0406@users.noreply.github.com>
2021-07-08 00:51:07 +0800

    Update EmployeeManagement.java

--- src/EmployeeManagement.java
+++ src/EmployeeManagement.java
@@ -1,40 +1,184 @@
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
-public class EmployeeManagement extends JFrame {
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.Color;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-	private JPanel contentPane;
+public class EmployeeManagement extends JPanel {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTable table;
 
 	/**
-	 * Launch the application.
+	 * Create the panel.
 	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					EmployeeManagement frame = new EmployeeManagement();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
+	public EmployeeManagement() {
+		
+		setSize(870, 583);
+		setLayout(null);
+		setVisible(true);
+		
+		JLabel lblEmployeeManagement = new JLabel("Employee Management");
+		lblEmployeeManagement.setBounds(259, 22, 363, 43);
+		lblEmployeeManagement.setFont(new Font("Tahoma", Font.PLAIN, 35));
+		add(lblEmployeeManagement);
+		
+		JLabel lblName = new JLabel("Name:");
+		lblName.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblName.setBounds(40, 138, 151, 31);
+		add(lblName);
+		
+		JLabel lblPosition = new JLabel("Position:");
+		lblPosition.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblPosition.setBounds(40, 204, 151, 31);
+		add(lblPosition);
+		
+		JLabel lblSalary = new JLabel("Salary:");
+		lblSalary.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblSalary.setBounds(40, 273, 151, 31);
+		add(lblSalary);
+		
+		JLabel lblContact = new JLabel("Contact:");
+		lblContact.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblContact.setBounds(40, 346, 151, 31);
+		add(lblContact);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		textField.setColumns(10);
+		textField.setBounds(168, 126, 170, 43);
+		add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		textField_1.setColumns(10);
+		textField_1.setBounds(168, 198, 170, 43);
+		add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		textField_2.setColumns(10);
+		textField_2.setBounds(168, 267, 170, 43);
+		add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		textField_3.setColumns(10);
+		textField_3.setBounds(168, 340, 170, 43);
+		add(textField_3);
+		
+		JButton btnAdd = new JButton("Add");
+		btnAdd.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DefaultTableModel model = (DefaultTableModel) table.getModel();
+				model.addRow(new Object[]{
+						textField.getText(),
+						textField_1.getText(),
+						textField_2.getText(),
+						textField_3.getText(),
+				});
+				
+				if (table.getSelectedRow() == -1) {
+					if (table.getRowCount() == 0) {
+						JOptionPane.showMessageDialog(null, "Employee Info Added", "Admin - Employee Management",
+								JOptionPane.OK_OPTION);
+					}
+				}
+				}
+			}
+		);
+		btnAdd.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnAdd.setBounds(24, 442, 151, 42);
+		add(btnAdd);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DefaultTableModel model = (DefaultTableModel)table.getModel();
+				if(table.getSelectedRow()==-1) {
+					if(table.getRowCount()==0) {
+						JOptionPane.showMessageDialog(null,"No data to delete",
+								"Admin - Employee Management", JOptionPane.OK_OPTION);
+					}else {
+						JOptionPane.showMessageDialog(null,"Select a row to delete",
+								"Admin - Employee Management", JOptionPane.OK_OPTION);
+					}
+				}else {
+					model.removeRow(table.getSelectedRow());
 				}
 			}
 		});
-	}
+		btnDelete.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnDelete.setBounds(24, 504, 151, 42);
+		add(btnDelete);
+		
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				if (table.getSelectedRow() == -1) {
+					if ((table.getRowCount() == 0)) {
+						JOptionPane.showMessageDialog(null, "No data to edit",
+								"Admin - Employee Management", JOptionPane.INFORMATION_MESSAGE);
+					} else {
+						JOptionPane.showMessageDialog(null, "Select a row to edit",
+								"Admin - Employee Management", JOptionPane.OK_OPTION);
+					}
+				} else {
+					table.setValueAt(textField.getText(), table.getSelectedRow(), 0);
+					table.setValueAt(textField_1.getText(), table.getSelectedRow(), 1);
+					table.setValueAt(textField_2.getText(), table.getSelectedRow(), 2);
+					table.setValueAt(textField_3.getText(), table.getSelectedRow(), 3);
+					
+				}
+			}
+		});
+		btnUpdate.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnUpdate.setBounds(197, 442, 151, 42);
+		add(btnUpdate);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				textField.setText("");
+				textField_1.setText("");
+				textField_2.setText("");
+				textField_3.setText("");
+			}
+		});
+		btnClear.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnClear.setBounds(197, 504, 151, 42);
+		add(btnClear);
+		
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.setBackground(Color.WHITE);
+		panel.setBounds(387, 101, 447, 457);
+		add(panel);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(10, 10, 427, 437);
+		panel.add(scrollPane_1);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+				{null, null, null, null},
+			},
+			new String[] {
+				"Name", "Position", "Salary", "Contact"
+			}
+		));
+		scrollPane_1.setViewportView(table);
 
-	/**
-	 * Create the frame.
-	 */
-	public EmployeeManagement() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
 	}
-
 }
