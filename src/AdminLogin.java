58af017de4f38442fcff24e25c705a894bfd0667
Ho Wee Sern <72723621+hws0406@users.noreply.github.com>
2021-07-08 00:49:09 +0800

    Update AdminLogin.java

--- src/AdminLogin.java
+++ src/AdminLogin.java
@@ -3,16 +3,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 
 import java.awt.Font;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JTextField;
 import javax.swing.JPasswordField;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 public class AdminLogin extends JFrame {
@@ -47,37 +48,31 @@ public class AdminLogin extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
-		JPanel contentPane_1 = new JPanel();
-		contentPane_1.setLayout(null);
-		contentPane_1.setBackground(Color.WHITE);
-		contentPane_1.setBounds(0, 0, 784, 548);
-		contentPane.add(contentPane_1);
-		
+	
 		JLabel lblAdminLoginPortal = new JLabel("Admin Login Portal");
 		lblAdminLoginPortal.setFont(new Font("Tahoma", Font.PLAIN, 35));
 		lblAdminLoginPortal.setBounds(210, 124, 353, 43);
-		contentPane_1.add(lblAdminLoginPortal);
+		contentPane.add(lblAdminLoginPortal);
 		
 		JLabel usernameLabel = new JLabel("Username:");
 		usernameLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		usernameLabel.setBounds(35, 265, 151, 31);
-		contentPane_1.add(usernameLabel);
+		contentPane.add(usernameLabel);
 		
 		JLabel passwordLabel = new JLabel("Password:");
 		passwordLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		passwordLabel.setBounds(35, 327, 151, 31);
-		contentPane_1.add(passwordLabel);
+		contentPane.add(passwordLabel);
 		
 		usernameField = new JTextField();
 		usernameField.setColumns(10);
 		usernameField.setBounds(210, 265, 170, 43);
-		contentPane_1.add(usernameField);
+		contentPane.add(usernameField);
 		
 		passwordField = new JPasswordField();
 		passwordField.setColumns(10);
 		passwordField.setBounds(210, 327, 170, 43);
-		contentPane_1.add(passwordField);
+		contentPane.add(passwordField);
 		
 		JButton loginButton = new JButton("Login");
 		loginButton.addActionListener(new ActionListener() {
@@ -93,7 +88,7 @@ public class AdminLogin extends JFrame {
 		});
 		loginButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		loginButton.setBounds(447, 266, 170, 42);
-		contentPane_1.add(loginButton);
+		contentPane.add(loginButton);
 		
 		JButton resetButton = new JButton("Reset");
 		resetButton.addActionListener(new ActionListener() {
@@ -104,7 +99,7 @@ public class AdminLogin extends JFrame {
 		});
 		resetButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		resetButton.setBounds(447, 328, 170, 42);
-		contentPane_1.add(resetButton);
+		contentPane.add(resetButton);
 		
 		JButton backButton = new JButton("Back");
 		backButton.addActionListener(new ActionListener() {
@@ -116,10 +111,16 @@ public class AdminLogin extends JFrame {
 			});
 		backButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		backButton.setBounds(664, 10, 98, 42);
-		contentPane_1.add(backButton);
+		contentPane.add(backButton);
 		backButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		backButton.setBounds(664, 10, 98, 42);
-		contentPane_1.add(backButton);
+		contentPane.add(backButton);
+		
+		JLabel backgroundLabel = new JLabel("");
+		backgroundLabel.setBounds(0, 0, 1563, 1080);
+		contentPane.add(backgroundLabel);
+		Image img1 = new ImageIcon(this.getClass().getResource("turquoise.jpg")).getImage();
+		backgroundLabel.setIcon(new ImageIcon(img1));
 	}
 
 }
