f5726a7b25b49cce7556abde1f2fbd27af7b5314
Ho Wee Sern <72723621+hws0406@users.noreply.github.com>
2021-07-08 00:49:42 +0800

    Update AdminPanel.java

--- src/AdminPanel.java
+++ src/AdminPanel.java
@@ -1,25 +1,29 @@
 import java.awt.EventQueue;
 import java.awt.Image;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 import javax.swing.JOptionPane;
 
-import java.awt.Font;
-import java.awt.Color;
+import java.awt.CardLayout;
 import javax.swing.JButton;
-import javax.swing.SwingConstants;
-import javax.swing.JLayeredPane;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
+import javax.swing.ImageIcon;
 
 public class AdminPanel extends JFrame {
 
 	private JPanel contentPane;
+	private JLayeredPane layeredPane;
 
 	/**
 	 * Launch the application.
@@ -36,67 +40,87 @@ public class AdminPanel extends JFrame {
 			}
 		});
 	}
+	
+	public void switchPanels(JPanel panel) {
+		layeredPane.removeAll();
+		layeredPane.add(panel);
+		layeredPane.repaint();
+		layeredPane.revalidate();
+		}
 
 	/**
 	 * Create the frame.
 	 */
 	public AdminPanel() {
+		contentPane = new JPanel();
+		setContentPane(contentPane);
+		contentPane.setBackground(Color.WHITE);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 899, 640);
-		getContentPane().setLayout(null);
+		setBounds(100, 100, 1122, 640);
+		contentPane.setLayout(null);
 		
-		JButton anmButton = new JButton("<html><p style=\\\"text-align:center;\\\">Advertisement<br />& Marketing</p></html>");
-		anmButton.addActionListener(new ActionListener() {
+		layeredPane = new JLayeredPane();
+		layeredPane.setBounds(228, 10, 870, 583);
+		contentPane.add(layeredPane);
+		layeredPane.setLayout(new CardLayout(0, 0));
+		
+		AdvertisementandMarketing panelAds = new AdvertisementandMarketing();
+		layeredPane.add(panelAds, "name_266983109842400");
+		
+		InventoryManagement panelInv = new InventoryManagement();
+		layeredPane.add(panelInv, "name_267021903240800");
+		
+		EmployeeManagement panelEmp = new EmployeeManagement();
+		layeredPane.add(panelEmp, "name_267023450419000");
+		
+		Finance panelFin = new Finance();
+		layeredPane.add(panelFin, "name_267024815719900");
+		
+		JButton adsButton = new JButton("<html><p style=\\\"text-align:center;\\\">Advertisement<br />& Marketing</p></html>");
+		adsButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				AdvertisementandMarketing aM = new AdvertisementandMarketing();
-				aM.setVisible(true);
-				setVisible(false);
+				switchPanels(panelAds);
 			}
 		});
-		anmButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		anmButton.setBounds(23, 75, 167, 60);
-		getContentPane().add(anmButton);
+		adsButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		adsButton.setBounds(23, 75, 167, 60);
+		contentPane.add(adsButton);
 		
-		JButton imButton = new JButton("<html><p style=\\\"text-align:center;\\\">Inventory<br />Management</p></html>");
-		imButton.addActionListener(new ActionListener() {
+		JButton invButton = new JButton("<html><p style=\\\"text-align:center;\\\">Inventory<br />Management</p></html>");
+		invButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				InventoryManagement iM = new InventoryManagement();
-				iM.setVisible(true);
-				setVisible(false);
+				switchPanels(panelInv);
 			}
 		});
-		imButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		imButton.setBounds(23, 174, 167, 60);
-		getContentPane().add(imButton);
+		invButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		invButton.setBounds(23, 174, 167, 60);
+		contentPane.add(invButton);
 		
-		JButton emButton = new JButton("<html><p style=\\\"text-align:center;\\\">Employee<br />Management</p></html>");
-		emButton.addActionListener(new ActionListener() {
+		JButton empButton = new JButton("<html><p style=\\\"text-align:center;\\\">Employee<br />Management</p></html>");
+		empButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				EmployeeManagement eM = new EmployeeManagement();
-				eM.setVisible(true);
-				setVisible(false);
+				switchPanels(panelEmp);
 			}
 		});
-		emButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		emButton.setBounds(23, 272, 167, 60);
-		getContentPane().add(emButton);
+		empButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		empButton.setBounds(23, 272, 167, 60);
+		contentPane.add(empButton);
 		
-		JButton fButton = new JButton("Finance");
-		fButton.addActionListener(new ActionListener() {
+		JButton finButton = new JButton("Finance");
+		finButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				Finance fA = new Finance();
-				fA.setVisible(true);
-				setVisible(false);
+				switchPanels(panelFin);
 			}
 		});
-		fButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		fButton.setBounds(23, 359, 167, 60);
-		getContentPane().add(fButton);
+		finButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		finButton.setBounds(23, 367, 167, 60);
+		contentPane.add(finButton);
 		
 		JButton backButton = new JButton("Back");
 		backButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if (JOptionPane.showConfirmDialog(null, "Are you sure to exit Admin Panel?", "Exit", 
+				JFrame frame = new JFrame();
+				if (JOptionPane.showConfirmDialog(null, "Are you sure to exit to Admin Panel?", "Exit", 
 						JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION) {
 					AdminLogin aLogin = new AdminLogin();
 					aLogin.setVisible(true);
@@ -105,13 +129,21 @@ public class AdminPanel extends JFrame {
 			}
 		});
 		backButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		backButton.setBounds(23, 448, 167, 60);
-		getContentPane().add(backButton);
+		backButton.setBounds(23, 462, 167, 60);
+		contentPane.add(backButton);
 		
 		JLabel backgroundLabel = new JLabel("");
+		backgroundLabel.setBounds(217, 0, 1563, 1080);
+		contentPane.add(backgroundLabel);
 		Image img1 = new ImageIcon(this.getClass().getResource("matte.jpg")).getImage();
-	    backgroundLabel.setIcon(new ImageIcon(img1));
-		backgroundLabel.setBounds(0, 0, 885, 603);
-		getContentPane().add(backgroundLabel);
+		backgroundLabel.setIcon(new ImageIcon(img1));
+		
+		JLabel backgroundLabel1 = new JLabel("");
+		backgroundLabel1.setBounds(0, 0, 218, 603);
+		contentPane.add(backgroundLabel1);
+		Image img2 = new ImageIcon(this.getClass().getResource("mosaic2.jpg")).getImage();
+		backgroundLabel1.setIcon(new ImageIcon(img2));
+		
+
 	}
 }
